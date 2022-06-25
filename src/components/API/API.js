bc3797cf60aaf52108342ee31e476183d0c61423
RomaYourchuk <yourchuk922@gmail.com>
2022-06-25 23:39:53 +0300

    test

---
+++ src/components/API/API.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const ServiceAPI = (q, page) => {
+  const options = {
+    params: {
+      key: 'e900ddd99edc3affd146f1905e638fd1',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 12,
+      q,
+      page,
+    },
+  };
+
+  return axios.get('https://pixabay.com/api/', options);
+};
+
+export { ServiceAPI };
